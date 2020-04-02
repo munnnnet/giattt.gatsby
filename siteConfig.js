2cbdef4e46fd91b601e9561f74dcfe4e4521c926
Fatul <33377537+KontenaIndonesia@users.noreply.github.com>
2020-04-02 08:58:47 +0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
     postsPerPage: 14, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Showcase of Giattt.com`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Create more. More fun!`, // This allows an alternative site description for meta data for pages.
+    siteDescriptionMeta: `Get unlimited graphic design for a flat-rates monthly fee. Choose $99/15 design concepts or $309/3 months. More than 1000 brands trust Giattt.com to design their Display ads, Print or digital brochures, T-shirt graphics, Trade show marketing materials, Facebook ad graphics, Instagram graphics, Google display ads, Social media quotes, Promotional items, Outdoor graphics, Stickers, Sales sheets, Event flyers, Email header graphics, Blog header graphics, “Hero” images for your website, Basic photo edits & manipulation, Knocking out backgrounds, Ecommerce graphics, Logo updates, Presentation graphics, Revising webpage mockup, Simple infographics, and more.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
